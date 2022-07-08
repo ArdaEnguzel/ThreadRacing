c9fa8d00b949330900efade35d7d3195c2929131
Arda Engüzel <arda.enguzel3@gmail.com>
2022-07-09 02:10:11 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Main {
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String args[]){
+        ArrayList<Integer> numbers=new ArrayList<>();
+        for(int i=0;i<1000;i++){
+            numbers.add(i);
+        }
+        ExecutorService pool= Executors.newFixedThreadPool(4);
+        Threads th= new Threads(numbers);
+        Thread ths=new Thread(th);
+        pool.execute(ths);
+        ths.interrupt();
+
+    }
 }
--- src/Threads.java
+++ src/Threads.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Threads {
+import java.util.*;
+public class Threads implements Runnable{
+    private ArrayList<Integer> even=new ArrayList<>();
+    private ArrayList<Integer> odd=new ArrayList<>();
+    private ArrayList<Integer> all;
+    public Threads(ArrayList<Integer> all){
+        this.all=all;
+    }
+    @Override
+    public void run(){
+
+        try{
+
+            for(int i=0;i<all.size();i++){
+                if(i%2==0){
+                    this.addOdd(i);
+                }
+                else{
+                    this.addEven(i);
+                }
+                System.out.println(even.size());
+                System.out.println(odd.size());
+                Thread.sleep(5);
+            }
+        }
+        catch(InterruptedException e){
+            System.out.println("Threads are stopped");
+        }
+
+
+
+    }
+    public synchronized void addOdd(int i){
+        odd.add(i);
+    }
+    public synchronized void addEven(int i){
+        even.add(i);
+    }
+    public void getOdd(){
+        System.out.println(odd);
+    }
+    public void getEven(){
+        System.out.println(even);
+    }
+
 }
